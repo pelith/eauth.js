807aed4db7c220848d1bda74c36bd3a3fbac9fa1
Gilg4mesh <freedomtomdestiny@gmail.com>
2019-08-14 23:09:51 +0800

    support eip-1271 with personal sign

--- src/eauth.js
+++ src/eauth.js
@@ -6,6 +6,7 @@ class Eauth {
         this.OAUTH_STATE = options.OAUTH_STATE
 
         this.AUTH_ROUTE = options.AUTH_ROUTE // domain + '/routeName'
+        this.CONTRACT_AUTH_ROUTE = options.CONTRACT_AUTH_ROUTE // domain + '/routeName'
         this.REDIRECT_URI = options.REDIRECT_URI
         this.AUTH_RESPONSE = null
     }
@@ -80,6 +81,64 @@ class Eauth {
                     .then((res) => { return res.json() })
                     .then((res) => {
                         this.AUTH_RESPONSE = res
+                        callback()
+                    })
+                }
+                return console.error('Missing arguments')
+            })
+        })
+    }
+
+    contractEthLogin(contractAddr, callback = () => { window.location.reload() }) {
+        if (typeof web3 !== 'undefined') {
+            console.log('web3 is detected.')
+        } else {
+            return alert('No web3 detected.')
+        }
+
+        if (web3.currentProvider.enable) {
+            web3.currentProvider.enable()
+                .then(() => {
+                    if (web3.eth.accounts[0] === undefined) {
+                        return alert('Please login your wallet extension first.')
+                    }
+                    this.walletValidation(web3, contractAddr, callback)
+                })
+        } else if (web3.eth.accounts[0]) {
+            this.walletValidation(web3, contractAddr, callback)
+        }
+    }
+
+    walletValidation(_web3, contractAddr, callback) {
+        return fetch(this.CONTRACT_AUTH_ROUTE + '/' + contractAddr, { method: 'get' }).then(res => {
+            return res.text()
+        })
+        .then(res => {
+            let data = ''
+            let message = ''
+            let method = 'personal_sign'
+            data = '0x' + Array.from(res).map(x => x.charCodeAt(0).toString(16)).join('')
+            message = res
+
+            // Call wallet extension to sign
+            const from = _web3.eth.accounts[0]
+            const params = [data, from]
+            _web3.currentProvider.sendAsync({
+                id: 1,
+                method,
+                params,
+            }, async (err, result) => {
+                if (err) return console.error(err)
+                if (result.error) return console.error(result.error)
+
+                const signature = result.result
+
+                if (message !== null && signature !== null) {
+                    return fetch(this.CONTRACT_AUTH_ROUTE + '/' + message + '/' + signature, { method: 'post' })
+                    .then((res) => { return res.text() })
+                    .then((res) => {
+                        this.AUTH_RESPONSE = res
+                        console.log(res)
                         callback()
                     })
                 }
