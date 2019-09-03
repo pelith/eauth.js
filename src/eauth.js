1b6696ab4323ead25dfdab9cf6d0cc3465cfa33a
Gilg4mesh <freedomtomdestiny@gmail.com>
2019-09-03 21:49:32 +0800

    remove latency & use async & pass account directly

--- src/eauth.js
+++ src/eauth.js
@@ -16,42 +16,34 @@ class Eauth {
         window.location = `${this.OAUTH_URL}?client_id=${this.OAUTH_CLIENT_ID}&redirect_uri=${this.OAUTH_REDIRECT_URI}&response_type=code&state=${this.OAUTH_STATE}`
     }
 
-    fortmaticLogin(callback = () => { window.location.reload() }) {
+    async fortmaticLogin(callback = () => { window.location.reload() }) {
         const fm = new Fortmatic('pk_live_CC75CEEE7D7E8630')
         const fortmaticWeb3 = new Web3(fm.getProvider())
-        fortmaticWeb3.currentProvider.enable()
-            .then(() => {
-                this.authStart(fortmaticWeb3, callback)
-            })
+        await fortmaticWeb3.currentProvider.enable()
+        .then(async (accounts) => {
+            this.authStart(fortmaticWeb3, accounts[0], callback)
+        })
     }
 
-    ethLogin(callback = () => { window.location.reload() }) {
+    async ethLogin(callback = () => { window.location.reload() }) {
         if (typeof web3 !== 'undefined') {
             console.log('web3 is detected.')
         } else {
             return alert('No web3 detected.')
         }
 
-        if (web3.currentProvider.enable) {
-            web3.currentProvider.enable()
-                .then(() => {
-                    if (web3.eth.accounts[0]) {
-                        this.authStart(web3, callback)
-                    } else {
-                        setTimeout(this.authStart(web3, callback), 1000)
-                    }
-                })
-        } else if (web3.eth.accounts[0]) {
-            this.authStart(web3, callback)
-        }
+        await web3.currentProvider.enable()
+        .then(async (accounts) => {
+            this.authStart(web3, accounts[0], callback)
+        })
     }
 
-    authStart(_web3, callback) {
-        if (!/^(0x)?[0-9a-f]{40}$/i.test(_web3.eth.accounts[0])) {
+    authStart(_web3, account, callback) {
+        if (!/^(0x)?[0-9a-f]{40}$/i.test(account)) {
             return alert('Wallet not detected.')
         }
 
-        return fetch(this.AUTH_ROUTE + '/' + _web3.eth.accounts[0], { method: 'get' }).then(res => {
+        return fetch(this.AUTH_ROUTE + '/' + account, { method: 'get' }).then(res => {
             return res.text()
         })
         .then(res => {
@@ -71,9 +63,10 @@ class Eauth {
                 data = prefixedRes
             }
 
-            // Call wallet extension to sign
-            const from = _web3.eth.accounts[0]
-            const params = [data, from]
+            if (!/^[a-fA-F0-9]+$/.test(message))
+                return alert('Something went wrong, please try again later.')
+
+            const params = [data, account]
             _web3.currentProvider.sendAsync({
                 id: 1,
                 method,
@@ -98,37 +91,29 @@ class Eauth {
         })
     }
 
-    contractEthLogin(contractAddr, callback = () => { window.location.reload() }) {
+    async contractEthLogin(contractAddr, callback = () => { window.location.reload() }) {
         if (typeof web3 !== 'undefined') {
             console.log('web3 is detected.')
         } else {
             return alert('No web3 detected.')
         }
 
-        if (web3.currentProvider.enable) {
-            web3.currentProvider.enable()
-                .then(() => {
-                    if (web3.eth.accounts[0]) {
-                        this.walletValidation(web3, contractAddr, callback)
-                    } else {
-                        setTimeout(this.walletValidation(web3, contractAddr, callback), 1000)
-                    }
-                })
-        } else if (web3.eth.accounts[0]) {
-            this.walletValidation(web3, contractAddr, callback)
-        }
+        await web3.currentProvider.enable()
+        .then((accounts) => {
+            this.walletValidation(web3, contractAddr, accounts[0], callback)
+        })
     }
 
-    contractFortmaticLogin(contractAddr, callback = () => { window.location.reload() }) {
+    async contractFortmaticLogin(contractAddr, callback = () => { window.location.reload() }) {
         const fm = new Fortmatic('pk_live_CC75CEEE7D7E8630')
         const fortmaticWeb3 = new Web3(fm.getProvider())
-        fortmaticWeb3.currentProvider.enable()
-            .then(() => {
-                this.walletValidation(fortmaticWeb3, contractAddr, callback)
-            })
+        await fortmaticWeb3.currentProvider.enable()
+        .then(async (accounts) => {
+            this.walletValidation(fortmaticWeb3, contractAddr, accounts[0], callback)
+        })
     }
 
-    walletValidation(_web3, contractAddr, callback) {
+    walletValidation(_web3, contractAddr, account, callback) {
         if (!/^(0x)?[0-9a-f]{40}$/i.test(contractAddr)) {
             return alert('Not a valid address.')
         }
@@ -153,9 +138,10 @@ class Eauth {
                 data = prefixedRes
             }
 
-            // Call wallet extension to sign
-            const from = _web3.eth.accounts[0]
-            const params = [data, from]
+            if (!/^[a-fA-F0-9]+$/.test(message))
+                return alert('Something went wrong, please try again later.')
+
+            const params = [data, account]
             _web3.currentProvider.sendAsync({
                 id: 1,
                 method,
@@ -189,6 +175,9 @@ class Eauth {
             return res.text()
         })
         .then(message => {
+            if (!/^[a-fA-F0-9]+$/.test(message))
+                return alert('Something went wrong, please try again later.')
+
             return this.PREFIX + message
         })
     }
